9a58e00f4d6c91c97d40bac19856c80966975c17
Owen Kim <tjs060708@gmail.com>
2025-02-01 19:12:24 -0500

    .1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,17 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.querySelector(".sidebar");
+    const arrowBtn = document.querySelector(".arrow-btn");
+    const menuBtn = document.querySelector(".menu-btn");
+
+    menuBtn.addEventListener("click", () => {
+        sidebar.classList.add("open");
+    });
+
+    arrowBtn.addEventListener("click", () => {
+        sidebar.classList.remove("open");
+    });
+});
