49a140c9fc0bf976eb5383bdf5f4ae4fbe8958c1
Owen Kim <tjs060708@gmail.com>
2025-02-01 20:21:47 -0500

    add js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,13 +7,13 @@ createApp(App).mount('#app')
 document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.querySelector(".sidebar");
     const arrowBtn = document.querySelector(".arrow-btn");
-    const menuBtn = document.querySelector(".menu-btn");
+    const menuBtn = document.getElementById("menu-btn");
 
     menuBtn.addEventListener("click", () => {
-        sidebar.classList.add("open");
+        side-bar.classList.add("open");
     });
 
     arrowBtn.addEventListener("click", () => {
-        sidebar.classList.remove("open");
+        side-bar.classList.remove("open");
     });
 });
